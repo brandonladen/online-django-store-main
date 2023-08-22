368eda9654f8119697c04fb111b0b6a09552981d
brandonladen <brandonladen486@gmail.com>
2023-08-22 22:16:08 +0300

    initial commit

---
+++ user/templatetags/custom_tags.py
@@ -0,0 +1,14 @@
+from django import template
+import datetime
+
+register = template.Library()
+
+
+@register.filter
+def addDays(days):
+    newDate = datetime.date.today() + datetime.timedelta(days=days)
+    return newDate
+ 
+@register.filter
+def FormatThousand(num):
+    return "{:,}".format(num)
